c60bdc981487be85f86a964fc4fe9d8c730a899c
AndersonBelleza <bellezatorresanderson@gmail.com>
2026-09-19 17:31:38 -0500

    Subiendo archivos properties

---
+++ src/main/java/com/example/upn_devops/controller/SaludoController.java
@@ -0,0 +1,15 @@
+package com.example.upn_devops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SaludoController {
+
+    @GetMapping("/saludo")
+    public String enviarSaludo() {
+        return "¡Hola! Saludos desde la API Web desarrollada para la UPN.";
+    }
+}
