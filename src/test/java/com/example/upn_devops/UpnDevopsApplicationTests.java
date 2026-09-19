0ef1cd901cc185cff24f47afb7b7ba5e69beefd9
AndersonBelleza <bellezatorresanderson@gmail.com>
2026-09-19 17:09:17 -0500

    first commit

---
+++ src/test/java/com/example/upn_devops/UpnDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upn_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpnDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
